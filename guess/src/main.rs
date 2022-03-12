b7c7f6fc35608a75f5f549d0d4eb1a7bc2caf1df
KAI ARNTZ <94131088+CirnoFumo9@users.noreply.github.com>
2022-03-12 04:15:11 +0000

    updates

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -5,23 +5,30 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
+    // declares the secret number using rand::thread_rng() with range 1-100
     let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
-    
+
+        // String is best if need to know and mutate a string
         let mut guess = String::new();
-    
+
+        // for user input
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-    
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    
+
+        // shadows String guess into u32 int
+        let guess: u32 = match guess.trim().parse() {
+            // parse() returns enum and determines action based on that. 
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
         println!("You guessed: {}", guess);
-    
+
+        // compares guess to secret_number
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
