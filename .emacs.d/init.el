171bc5eb7540542eda36844fade3a870e43f8f20
KAI ARNTZ <94131088+CirnoFumo9@users.noreply.github.com>
2022-03-20 06:30:14 +0000

    yay

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,7 @@
+;
+; Helpful sites:
+;	https://melpa.org/#/getting-started
+;
 
 ; Hide welcome screen
 (setq inhibit-startup-screen t)
@@ -6,3 +10,18 @@
 (add-to-list 'load-path "~/DarksalmonEllipticalDrawings/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
+
+; Sets up Melpa
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+; Sets up powerline
+(require 'powerline-evil)
+(powerline-default-theme)
+
+; Sets up theme
+(load-theme 'inkpot t)
+
+
+
