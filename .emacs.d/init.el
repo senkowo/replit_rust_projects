63d3ab80f556fe75e66651af1d4bba885691dc47
KAI ARNTZ <94131088+CirnoFumo9@users.noreply.github.com>
2022-03-20 15:20:52 +0000

    upd melpa

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,16 +12,18 @@
 (evil-mode 1)
 
 ; Sets up Melpa
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+;(require 'package)
+;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;(package-initialize)
 
 ; Sets up powerline
-(require 'powerline-evil)
-(powerline-default-theme)
+;(require 'powerline-evil)
+;(powerline-default-theme)
 
 ; Sets up theme
+(add-to-list 'custom-theme-load-path "~/DarksalmonEllipticalDrawings/.emacs.d/themes/")
 (load-theme 'inkpot t)
+
 
 
 
