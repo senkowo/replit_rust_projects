7c7964ec1f07f70c054b37a24b1e9ff09f8b12b4
KAI ARNTZ <94131088+CirnoFumo9@users.noreply.github.com>
2022-03-22 23:14:50 +0000

    maybe the issue was no chmod on startup.sh

---
+++ game1/src/main.rs
@@ -0,0 +1,12 @@
+struct Stats {
+    occupancy: bool,
+    // 1 and 2 for players 1 and 2 respectively, and 0 for no piece on the space.
+    team: i8,
+}
+
+fn main() {
+    println!("Hello, world!");
+    let mut v = Vec::new();
+}
+
+// for structs, use 1_2 as in (1, 2).
--- startup.sh
+++ startup.sh
@@ -3,4 +3,8 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "vim-plug installed!"
 
-if 
+if [ -d ~/.emacs.d ]; then 
+    a
+else
+    b
+fi
