43f2ba7a45e96006135ed4cdbfaa53fa48e491c4
KAI ARNTZ <94131088+CirnoFumo9@users.noreply.github.com>
2022-04-05 18:17:13 +0000

    impl, methods, and associated fn prac

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,19 +1,23 @@
-fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
-    let rect2 = Rectangle { width: 10, height: 40 };
-    let rect3 = Rectangle { width: 60, height: 45 };
-
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-}
-
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
+    fn area(&self) -> u32 {
+        self.width * self.height
     }
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle { width: 10, height: 30 };
+    println!("The area of rect1 is {}", rect1.area());
+
+    println!(
+        "The width of a square of 6 units long per side is {}",
+        Rectangle::square(6).width
+    )
 }
